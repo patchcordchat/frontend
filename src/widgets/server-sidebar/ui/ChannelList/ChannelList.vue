e8e3a7293551e56077ab5b7675214fb44d75aa18
vlkosh <pshift4180@gmail.com>
2026-02-04 01:38:41 +0300

    feat: обновлены стили и компоненты списка каналов для улучшения интерфейса
    - Добавлен новый компонент SectionDivider в ChannelList для разделения элементов.
    - Обновлены элементы списка с новыми названиями и типами.
    - Улучшены стили для ListItem, добавлены элементы управления и новые классы для управления отображением.

--- src/widgets/server-sidebar/ui/ChannelList/ChannelList.vue
+++ src/widgets/server-sidebar/ui/ChannelList/ChannelList.vue
@@ -1,19 +1,19 @@
 <template>
   <ul class="channel-list">
+    <section-divider />
+
+    <list-item name="test text" icon="misc.speaker" :type="0" />
+
     <list-group />
 
-    <list-item name="pron" icon="misc.speaker" :type="0" />
+    <list-item name="test voice" icon="misc.speaker" :type="1" />
 
-    <list-item name="pron1" icon="misc.speaker" :type="0" />
+    <list-item name="test video" icon="misc.speaker" :type="1" />
   </ul>
 </template>
 
 <script setup lang="ts">
 import ListItem from './ListItem.vue'
 import ListGroup from './ListGroup.vue'
+import SectionDivider from './SectionDivider.vue'
 </script>
-
-<style scoped lang="scss">
-.channel-list {
-}
-</style>
--- src/widgets/server-sidebar/ui/ChannelList/ListItem.vue
+++ src/widgets/server-sidebar/ui/ChannelList/ListItem.vue
@@ -4,7 +4,15 @@
       <p-icon :icon="icon" />
     </div>
 
-    <div>{{ props.name }}</div>
+    <div class="channel__name">
+      {{ props.name }}
+    </div>
+
+    <div class="channel__controls">
+      <p-icon class="channel__control" icon="misc.user-add" size="xs" />
+
+      <p-icon class="channel__control" icon="misc.gear" size="xs" />
+    </div>
   </li>
 </template>
 
@@ -28,20 +36,35 @@ const icon = computed(() => iconMap[props.type] ?? 'misc.hashtag')
 </script>
 
 <style scoped lang="scss">
-.channel {
+@use '@/app/styles/utils/mixins.scss' as mixins;
+
+$block: '.channel';
+
+#{$block} {
   position: relative;
   display: flex;
+  gap: var(--space-xs);
   align-items: center;
+  justify-content: center;
   padding: 0.375rem 0.5rem;
   padding-top: var(--space-xxs);
   padding-bottom: var(--space-xxs);
   box-sizing: border-box;
   border-radius: var(--radius-sm);
   cursor: pointer;
+  transition: opacity 0.2s ease-in-out;
   padding-inline: var(--space-xs);
 
   &:hover {
     background: var(--background-modifier-hover);
+
+    #{$block}__name {
+      color: var(--interactive-text-active);
+    }
+
+    #{$block}__control {
+      display: block;
+    }
   }
 
   &--active {
@@ -54,6 +77,35 @@ const icon = computed(() => iconMap[props.type] ?? 'misc.hashtag')
     justify-content: center;
     width: 1.25rem;
     height: 1.25rem;
+    color: var(--channel-icon);
+  }
+
+  &__name {
+    @include mixins.text-md-medium;
+
+    flex: 1 1 auto;
+    overflow: hidden;
+    color: var(--channels-default);
+    line-height: 1.5rem;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+  }
+
+  &__controls {
+    display: flex;
+    flex: 0 0 auto;
+    align-items: center;
+    justify-content: center;
+  }
+
+  &__control {
+    display: none;
+    margin-inline-start: var(--space-xxs);
+    color: var(--interactive-text-default);
+
+    &:hover {
+      color: var(--icon-strong);
+    }
   }
 }
 </style>
