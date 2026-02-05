0c90a6cc07892f74645001694b157c1623f629f2
vlkosh <pshift4180@gmail.com>
2026-02-05 17:55:29 +0300

    feat: обновлены стили и компоненты для улучшения интерфейса отправки сообщений
    - Изменены цвета фона и текста в темной теме для лучшего контраста.
    - Добавлен новый компонент MessageCardSkeleton для отображения загрузки сообщений.
    - Обновлен компонент MessageForm с новой структурой и стилями для улучшения пользовательского опыта.
    - Упрощены экспорты в index.ts для лучшей организации кода.

--- src/features/send-message/ui/MessageForm.vue
+++ src/features/send-message/ui/MessageForm.vue
@@ -1,5 +1,41 @@
 <template>
-  <form class="message-form"></form>
+  <form class="message-form">
+    <div class="message-form__inner">
+      <div class="message-form__attach-wrapper">
+        <p-icon class="message-form__attach-icon" icon="misc.attach" />
+      </div>
+
+      <textarea class="message-form__textarea" style="height: 56px"></textarea>
+
+      <div class="message-form__buttons"></div>
+    </div>
+  </form>
 </template>
 
 <script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.message-form {
+  position: relative;
+  flex-shrink: 0;
+  margin-top: -1rem;
+  padding-inline: var(--space-xs);
+  padding-inline-start: calc(var(--space-md) - 1px);
+
+  &__inner {
+    position: relative;
+    display: flex;
+    width: 100%;
+    margin-bottom: var(--space-xs);
+    text-indent: 0;
+    border: 1px solid var(--border-muted);
+    border-radius: var(--radius-sm);
+    background: var(--chat-background-default);
+    transition: border-color 0.2s ease;
+  }
+
+  &__textarea {
+    width: 100%;
+  }
+}
+</style>
